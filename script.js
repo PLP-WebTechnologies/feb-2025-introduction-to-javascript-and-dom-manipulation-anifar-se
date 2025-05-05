970ae2680f29981e3645310959bf1dd7531d86f5
anifar-se <80062683+anifar-se@users.noreply.github.com>
2025-05-05 22:01:26 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function changeText() {
+    document.getElementById("description").textContent = "Text has been changed!";
+}
+
+function toggleBox() {
+    let box = document.getElementById("box");
+    box.classList.toggle("hidden");
+}
